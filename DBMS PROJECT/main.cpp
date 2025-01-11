7aa1cbe76aa23412c012807162e3076200503317
Florin . <florin.melinte12@yahoo.com>
2025-01-11 14:41:25 +0200

    show index from all class

--- DBMS PROJECT/main.cpp
+++ DBMS PROJECT/main.cpp
@@ -2298,6 +2298,24 @@ public:
 		return showIndexFromTable(tableName);
 	}
 };
+class showIndexFromAll : public Command {
+private:
+public:
+	void execute(Database& db) override {
+		db.showIndexFromAll();
+	}
+	static showIndexFromAll parseCommand(const string& command) {
+		string commandCopy = command;
+		stringUtils::trim(commandCopy);
+
+		// Check if the command is "SHOW INDEX FROM ALL"
+		if (commandCopy != "SHOW INDEX FROM ALL") {
+			throw invalid_argument("Invalid command format.");
+		}
+
+		return showIndexFromAll();
+	}
+};
 
 class selectCommands : public Command {
 protected:
