1eedf746ea34cc51fb08c01dd00f12d9a031d1c7
Florin . <florin.melinte12@yahoo.com>
2025-01-16 19:02:30 +0200

    startCommands header

--- DBMS PROJECT/main.cpp
+++ DBMS PROJECT/main.cpp
@@ -6,51 +6,9 @@
 #include "Database.h"
 #include "Command.h"
 #include "commandParser.h"
+#include "startCommands.h"
 
 using namespace std;
-
-class startCommands {
-public:
-	static void processCommandFiles(commandParser& parser, Database& db, const string filePaths[], int fileCount) {
-		for (int i = 0; i < fileCount; ++i) {
-			if (filePaths[i].substr(filePaths[i].find_last_of(".") + 1) != "txt") {
-				cout << "File is not a .txt file: " << filePaths[i] << endl;
-				continue;
-			}
-
-			if (!std::filesystem::exists(filePaths[i])) {
-				cout << "File does not exist: " << filePaths[i] << endl;
-				continue;
-			}
-
-			ifstream file(filePaths[i]);
-			if (!file.is_open()) {
-				cout << "Error opening file: " << filePaths[i] << endl;
-				continue;
-			}
-
-			string command;
-			while (getline(file, command)) {
-				try {
-					Command* cmd = parser.handleCommand(command);
-					cmd->execute(db);
-					delete cmd;
-				}
-				catch (const invalid_argument& e) {
-					cout << "Error in file " << filePaths[i] << ": " << e.what() << endl;
-				}
-				catch (const exception& e) {
-					cout << "Error in file " << filePaths[i] << ": " << e.what() << endl;
-				}
-				catch (...) {
-					cout << "An error occurred in file " << filePaths[i] << endl;
-				}
-			}
-
-			file.close();
-		}
-	}
-};
 
 class readSettingsFile {
 public:
